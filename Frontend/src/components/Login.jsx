8bae6a6245899e02b1f5bab1845f426458312fd4
Geethanjali S <geethanjalis5657@gmail.com>
2024-09-22 16:04:19 +0530

    first commit

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <div className='container mx-auto px-4 '>
+halo
+      </div>
+    </div>
+  )
+}
+
+export default Login
